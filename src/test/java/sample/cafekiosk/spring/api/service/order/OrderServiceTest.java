76e82511a2d965437836aaa43a6efc50c5e64000
jujinyoung <jujinyoung1838@naver.com>
2024-01-02 22:25:44 +0900

    Presentation Layer 테스트

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -67,7 +67,7 @@ class OrderServiceTest {
         OrderCreateRequest request = OrderCreateRequest.builder()
                 .productNumbers(List.of("001", "002"))
                 .build();
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+        OrderResponse orderResponse = orderService.createOrder(request.toServiceRequest(), registeredDateTime);
 
         //then
         assertThat(orderResponse.getId()).isNotNull();
@@ -101,7 +101,7 @@ class OrderServiceTest {
                 .build();
 
         //when
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+        OrderResponse orderResponse = orderService.createOrder(request.toServiceRequest(), registeredDateTime);
 
         //then
         assertThat(orderResponse.getId()).isNotNull();
@@ -146,7 +146,7 @@ class OrderServiceTest {
                 .build();
 
         //when //then
-        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+        assertThatThrownBy(() -> orderService.createOrder(request.toServiceRequest(), registeredDateTime))
                 .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("재고가 부족한 상품이 있습니다.");
     }
@@ -166,7 +166,7 @@ class OrderServiceTest {
                 .build();
 
         //when
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+        OrderResponse orderResponse = orderService.createOrder(request.toServiceRequest(), registeredDateTime);
 
         //then
         assertThat(orderResponse.getId()).isNotNull();
